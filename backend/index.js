bad7b2d3d1c55e53e9977ff1260fbdf54bb61e99
crosscay <caicedocarlos0306@gmail.com>
2022-12-12 18:42:51 -0500

    Routing de usuarios

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import conectarDB from './config/db.js';
 import dotenv from "dotenv";
+import usuarioRoutes from './routes/usuarioRoutes.js';
 
 
 
@@ -11,9 +12,12 @@ dotenv.config();
 
 conectarDB();
 
-// console.log("Sigue el mismo ");
-// console.log("Sigue el mismo ");
+// Routing
+app.use("/api/usuarios", usuarioRoutes);
+
+
 const PORT = process.env.PORT || 4000;
+
 app.listen(PORT, ()=>{
     console.log(`Servidor corriendo en puerto ${PORT}`);
 });
