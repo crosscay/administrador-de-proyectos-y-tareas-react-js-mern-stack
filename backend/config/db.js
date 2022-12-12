25bbec41654362410b616ef03f014c8af6e9bf38
crosscay <caicedocarlos0306@gmail.com>
2022-12-12 13:35:48 -0500

    Ocultando la conexión de la BD en variables de entorno

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const conectarDB =  async ()=>
 {
     try {
-        const connection = await mongoose.connect(`mongodb+srv://root:18623698@cluster0.xrbtysk.mongodb.net/uptask?retryWrites=true&w=majority`,
+        const connection = await mongoose.connect(process.env.MONGO_URI,
         {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,19 @@
 // const express = require('express'); 
 import express from 'express';
 import conectarDB from './config/db.js';
+import dotenv from "dotenv";
+
+
 
 const app = express();
 
-const port = 4000;
+dotenv.config();
 
 conectarDB();
 
 // console.log("Sigue el mismo ");
 // console.log("Sigue el mismo ");
-app.listen(port, ()=>{
-    console.log("Servidor corriendo en puerto " + port);
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, ()=>{
+    console.log(`Servidor corriendo en puerto ${PORT}`);
 });
